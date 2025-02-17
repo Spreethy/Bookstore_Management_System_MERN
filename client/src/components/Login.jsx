f63bc5f9ee90627a3d5e9f8b16fb9e0ccf34de3d
Spreethy <90103234+Spreethy@users.noreply.github.com>
2025-02-17 09:44:39 -0800

    Updated Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import '../css/Login.css'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className = 'login-page'>
+      <div className="login-container">
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input type = "text" placeholder ="Enter Username"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type = "text" placeholder ="Enter Password"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="role">Role:</label>
+          <select name="role" id="role">
+            <option value="admin">Admin</option>
+            <option value="student">Student</option>
+          </select>
+        </div>
+        <button className='btn-login'>Login</button>
+      </div>
+    </div>
   )
 }
 
